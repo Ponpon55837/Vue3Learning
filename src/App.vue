4e09f6aec0ce40f4de9c4b6a4e52c9eeb127bab7
Upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2020-12-18 15:28:36 +0800

    add new project for me to learning vue3.0

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal :theme='theme' @close="toggleModal" >
+      <!-- 以default slot方式傳入內容，原先則是以:header='header'方式傳入 -->
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+      <!-- 以命名為links的slot來傳入這個特別的slot -->
+      <template v-slot:links>
+        <a href='#'>AAA</a>
+        <a href='#'>BBB</a>
+      </template>
+    </Modal>
+  </div>
+  <button @click="toggleModal">Toggle Modal</button>
+  <button @click="toggleTheme">Toggle Modal</button>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'APP',
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: 'My first Vue App',
+      header: 'Sign of the data',
+      text: 'Grab your web for half price',
+      showModal: false,
+      theme: 'light'
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleTheme() {
+      this.theme = 'light' ? this.theme = 'dark' : this.theme = 'light'
+      console.log(this.theme)
+    }
+  }
+}
+</script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class='backdrop' @click='closeModal'>
+    <!-- 這邊的sale是要被綁定的class，theme則是用作爲判斷值的value，theme從父元件傳入 -->
+    <div class='modal' :class="{ sale: theme === 'dark' }">
+      <!-- {{}} 當中的內容都由父元件傳入 -->
+      <!-- <h1>{{ header }}</h1>
+      <p>{{ text }}</p> -->
+      <slot>No such value input  in Default Slot</slot>
+      <slot name='links'>No such value input  in Links Slot</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['header', 'text', 'theme'],
+  methods: {
+    closeModal() {
+      // $emit 可以從父元件拿到function來用
+      this.$emit('close')
+    }
+  }
+}
+</script>
