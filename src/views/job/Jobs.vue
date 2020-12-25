fe832944d7f99a739866407c4df8fd3e8a305a87
Upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2020-12-25 15:41:12 +0800

    need to fix this data problem

--- src/views/job/Jobs.vue
+++ src/views/job/Jobs.vue
@@ -1,31 +1,28 @@
 <template>
   <h1>Jobs</h1>
-  <div class="job" v-for="job in jobs" key='job.id' :title='job.title'>
-    <router-link
-      :to="{
-        name: 'JobsDetails',
-        params: { id: job.id, details: job.details }
-      }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div class="job" v-for="wave in waves" key='wave.stationId' :title='wave.locationName'>
+    <h2>{{ wave.locationName }}</h2>
   </div>
+  {{ waves.validTime }}
 </template>
 
 <script>
 import JobsDetails from './JobsDetails.vue'
 
 export default {
-  components: {
-    JobsDetails
-  },
   data() {
     return {
-      jobs: [
-        { title: 'UX DESIGNER', id: 1, details: 'make by designer' },
-        { title: 'WEB DEVELOPER', id: 2, details: 'make by developer' },
-        { title: 'WEB USER', id: 1, details: 'use by user' },
-      ]
+      waves: []
     }
+  },
+  mounted() {
+    fetch(`https://opendata.cwb.gov.tw/api//v1/rest/datastore/F-A0021-001?Authorization=CWB-59284856-0F85-4E19-A05B-42EDCCC4B575`)
+    .then(res => res.json())
+    .then(data => {
+      const resdata = data.records.location[0]
+      this.waves = resdata
+    })
+    .catch(err => console.log(err.message))
   }
 }
 </script>
