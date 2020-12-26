2713c72fc4cd782bf2a9f53f1dbf18fc3c26cf16
Upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2020-12-26 14:29:31 +0800

    add nav bar to be opacity with scroll

--- src/outerComponents/Nav.vue
+++ src/outerComponents/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="nav">
+  <div id="nav" @scroll='handleScroll' :class="{ opacityReduce: active }">
     <router-link :to="{ name: 'Home', params: {} }">Home</router-link>
     <router-link :to="{ name: 'About', params: {} }">About</router-link>
     <router-link :to="{ name: 'Jobs', params: {} }">Jobs</router-link>
@@ -8,6 +8,18 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      active: false
+    }
+  },
+  created() {
+      window.addEventListener("scroll", this.handleScroll);
+    },
+  methods: {
+    handleScroll() {
+      this.active = window.scrollY > 20 ? true : false
+    }
+  }
 }
 </script>
